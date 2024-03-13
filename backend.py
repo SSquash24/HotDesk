14864894827e8f5cd71501bfed1a5a3076d25bad
EternalTempest <shane.ong.kheng.te@gmail.com>
2024-03-14 00:55:37 +0800

    Adapt Flask example to FastAPI

--- backend.py
+++ backend.py
@@ -1,66 +1,52 @@
-# Backend Server that communicates with Front end
-# Urls:
-#   /       - Home page         - REQUIRES LOGIN
-#   /login  - Login page
-#   /book   - Booking page      - REQUIRES LOGIN
-
-# POST Urls:
-#   /login      -FORM: Login details (currently just username)
-#   /logout
-#   /book       -FORM: Booking details (currently just date)
+# Backend API with FastAPI 
+# (I removed all the frontend endpoints with API "equivalents" for demonstration)
 
 
+from fastapi import FastAPI, Request, HTTPException
+from starlette.applications import Starlette
+from starlette.middleware import Middleware
+from starlette.middleware.sessions import SessionMiddleware
 
-# Run server locally with flask --app backend run
-# Make sure flask is installed! (easily done with pip)
-
-from flask import *
 from userDB import users_getUID, users_isValidUID, users_getUsername
 from officeDB import office_getBooking, office_tryBook
 
-from datetime import datetime
-
-
-app = Flask(__name__)
+from datetime import date, datetime
 
 # secret key used for encoding session data
-app.secret_key = b'QMBaG%&0w;JGJa1*8[Qm_2BpGCW'
+secret_key = b'QMBaG%&0w;JGJa1*8[Qm_2BpGCW'
 
+middleware = [
+    Middleware(SessionMiddleware, secret_key=secret_key, https_only=True)
+]
+
+app = FastAPI(middleware=middleware)
+
+
+
+def logout(session, forcedLogout = False):
+    session.clear()
+    if not forcedLogout:
+        return "Successfully logged out!"
+    return ""
 
 # PAGE URLS (get requests) ----------------------------------------------------------------
 
 
-@app.route("/")
-def mainPage():
+@app.get("/")
+async def root(req: Request):
 
     #validate login details. If invalid logout (will send to login page)
     uid = None
-    if 'uid' in session:
-        uid = session['uid']
+    if 'uid' in req.session:
+        uid = req.session['uid']
     
     if not users_isValidUID(uid):
-        return logoutPost(True)
+        logout(req.session, forcedLogout=True)
+        return {}
     
 
-    return render_template('index.html', booking= office_getBooking(uid), name = users_getUsername(uid))
+    return {"booking": office_getBooking(uid), "name": users_getUsername(uid)}
 
-@app.route("/login")
-def loginPage():
-    return render_template('login.html')
-
-
-@app.route("/book/")
-def bookPage():
-    
-    #validate login details. If invalid logout (will send to login page)
-    uid = None
-    if 'uid' in session:
-        uid = session['uid']
-    
-    if not users_isValidUID(uid):
-        return logoutPost(True)
-
-    return render_template('booking.html', name = users_getUsername(uid))
 
 
 
@@ -68,39 +54,27 @@ def bookPage():
 
 
 @app.post("/login")
-def loginPost():
-    uid = users_getUID([request.form['username']])
+def login(username: str, req: Request):
+    uid = users_getUID([username])
     if uid == -1:
-        flash("Invalid login credentials. Please try again!")
-        return redirect(url_for("loginPage"))
-
-    session['uid'] = uid
-    return redirect(url_for("mainPage"))
+        raise HTTPException(status_code=404, detail="Invalid login credentials. Please try again!")
+    req.session['uid'] = uid
+    return
 
 
 @app.post("/logout")
-def logoutPost(forcedLogout=False):
-    session.clear()
-    if not forcedLogout:
-        flash("Successfully logged out!")
-    return redirect(url_for("loginPage"))
+def logout_endpoint(req: Request):
+    return logout(req.session)
 
 @app.post("/book")
-def bookPost():
+def book(req: Request, booking_date: date):
 
     #get UID
     uid = None
-    if 'uid' in session:
-        uid = session['uid']
+    if 'uid' in req.session:
+        uid = req.session['uid']
 
-    error = office_tryBook(uid,
-                datetime.strptime(
-                    request.form['date'],
-                    '%Y-%m-%d').date()
-    )
-    if error == None:
-        flash(f"Successfully booked for {request.form['date']}")
-    else:
-        flash(f"Error: {error}")
-
-    return bookPage()
+    error = office_tryBook(uid, booking_date)
+    if error:
+        raise HTTPException(status_code=400, detail=f"Error: {error}")
+    return f"Successfully booked for {booking_date}"
