48a167dabeb3b4f0a7b2ddf05d7445fc01a30648
Joshua <joshuacoombe.jc@gmail.com>
2024-04-25 21:04:00 +0100

    Change pw now does api call

--- frontend/src/Account/Account.jsx
+++ frontend/src/Account/Account.jsx
@@ -2,12 +2,35 @@ import { useContext } from 'react';
 import { TokenContext } from '../Navigator/Navigator';
 import './account.css'
 
+
+
 function ChangePassword() {
+
+    const { token } = useContext(TokenContext)
 
     function handleClick() {
         let newPW = document.getElementById('pwInput').value
         if (window.confirm("Are you sure you want to change your password to '" + newPW + "' ?")) {
-            alert('YES')
+            fetch(global.config.api_path + "reset/password?password=" + newPW, {
+                method: "POST",
+                headers: {
+                    "Authorization": token,
+                    "accept": "application/json"
+                }
+            }).then(async (response) => {
+                if (response.ok) {
+                    alert("Password Successfully changed!")
+                } else {
+                    let json = await response.json();
+                    if (json.detail[0].msg === undefined) {
+                        alert(`Error: ${json.detail}`)
+                    } else {
+                        alert(`Error: ${json.detail[0].msg}`)
+                    }
+                }
+            }).catch((err) => {
+                alert("Error: Could not connect to server")
+            })
         }
     }
 
