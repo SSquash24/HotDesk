4b2c464c4ad8367caa4ec80899ac25a53de63790
EternalTempest <shane.ong.kheng.te@gmail.com>
2024-04-26 18:25:49 +0100

    Refactor to follow the FastAPI bigger application tutorial

---
+++ app/config.py
@@ -0,0 +1,9 @@
+# secret key used for encoding session data
+# should be in some .env file in production, not committed
+# but this works for now
+# to get a string like this run:
+# openssl rand -hex 32
+
+SECRET_KEY = "6d7b5968fccbc2b4d484280030d9424536091a64a9bd7c167e91ea18aaa98312"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
---
+++ app/dependencies.py
@@ -0,0 +1,56 @@
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
+from app import crud, schemas, config
+from app.database import SessionLocal
+from jose import JWTError, jwt
+
+
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+
+
+
+CREDENTIALS_EXCEPTION = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
+
+def get_db():
+    # return dummy_db
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def verify_token(token: Annotated[str, Depends(oauth2_scheme)]):
+    try:
+        payload = jwt.decode(
+            token, config.SECRET_KEY, algorithms=[config.ALGORITHM]
+        )
+        uid: str = payload.get("sub")
+        role: str = payload.get("role")
+        if uid is None or role is None:
+            raise CREDENTIALS_EXCEPTION
+        token_data = schemas.TokenData(uid=int(uid), role=role)
+    except JWTError:
+        raise CREDENTIALS_EXCEPTION
+    return token_data
+
+def get_current_user(
+    token_data: Annotated[str, Depends(verify_token)], db = Depends(get_db)
+):
+    user = crud.get_user(db, token_data.uid)
+    if user is None:
+        raise CREDENTIALS_EXCEPTION
+    return user
+
+def verify_admin(token_data: Annotated[str, Depends(verify_token)]):
+    if(token_data.role != "admin"):
+        raise CREDENTIALS_EXCEPTION
